5f76b5ae30b2252b3760318cb7700d083f61813e
TiennLevii <nguyentrachtien2401@gmail.com>
2023-11-29 23:06:52 +0700

    Update File sql

--- wd18331_duan1.sql
+++ wd18331_duan1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 29, 2023 lúc 08:42 AM
+-- Thời gian đã tạo: Th10 29, 2023 lúc 05:06 PM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -46,19 +46,21 @@ CREATE TABLE `binhluan` (
   `noidung` text NOT NULL,
   `iduser` int(11) NOT NULL,
   `idpro` int(11) NOT NULL,
-  `ngaybinhluan` varchar(255) NOT NULL
+  `ngaybinhluan` varchar(255) NOT NULL,
+  `trangthai` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `binhluan`
 --
 
-INSERT INTO `binhluan` (`id`, `noidung`, `iduser`, `idpro`, `ngaybinhluan`) VALUES
-(1, 'Sp ổn', 1, 6, '08:55:26am 27/11/2023'),
-(2, 'Hiện tại em đang học lập trình web, sang kỳ 2 em muốn đổi sang UDPM thì có phải học lại các môn kỳ 1 không ạ?', 3, 6, '08:55:57am 27/11/2023'),
-(3, 'Thái Quỳnh Cover ( Chẳng phải là tôi đâu ) |', 3, 1, '08:56:33am 27/11/2023'),
-(4, '66666666666', 1, 1, '04:25:41pm 28/11/2023'),
-(5, '1', 4, 1, '04:29:53pm 28/11/2023');
+INSERT INTO `binhluan` (`id`, `noidung`, `iduser`, `idpro`, `ngaybinhluan`, `trangthai`) VALUES
+(1, 'Sp ổn', 1, 6, '08:55:26am 27/11/2023', 1),
+(2, 'Hiện tại em đang học lập trình web, sang kỳ 2 em muốn đổi sang UDPM thì có phải học lại các môn kỳ 1 không ạ?', 3, 6, '08:55:57am 27/11/2023', 1),
+(3, 'Thái Quỳnh Cover ( Chẳng phải là tôi đâu ) |', 3, 1, '08:56:33am 27/11/2023', 1),
+(4, '66666666666', 1, 1, '04:25:41pm 28/11/2023', 1),
+(5, '1', 4, 1, '04:29:53pm 28/11/2023', 1),
+(6, 'RAmos', 4, 4, '12:44:34pm 29/11/2023', 1);
 
 -- --------------------------------------------------------
 
@@ -112,7 +114,9 @@ INSERT INTO `donhang` (`id`, `khachhang`, `diachi`, `sdt`, `email`, `thoigiandat
 (4, 'muller', 'Đan Phượng - Hà Nội', '0987231231', 'muller@gmail.com', '04:14:49am 25/11/2023', 1, 2, 0, 'Hàng dễ hỏng'),
 (5, 'muller', 'Đan Phượng - Hà Nội', '0987231231', 'muller@gmail.com', '04:15:11am 25/11/2023', 1, 0, 0, 'Hàng dễ hỏng'),
 (6, 'muller', 'Đan Phượng - Hà Nội', '0987231231', 'muller@gmail.com', '01:43:10pm 27/11/2023', 0, 2, 0, ''),
-(7, 'tienlienha123', 'Thanh Trì - Hà Nội', '09283812', 'tienlevi@gmail.com', '04:12:53pm 28/11/2023', 0, 1, 0, '');
+(7, 'tienlienha123', 'Thanh Trì - Hà Nội', '09283812', 'tienlevi@gmail.com', '04:12:53pm 28/11/2023', 0, 1, 0, ''),
+(8, 'rooney', 'Hà Nội', '09837213111', 'rooney@gmail.com', '12:34:24pm 29/11/2023', 0, 1, 0, ''),
+(9, 'rooney', 'Hà Nội', '09837213111', 'rooney@gmail.com', '12:35:50pm 29/11/2023', 0, 1, 0, '');
 
 -- --------------------------------------------------------
 
@@ -136,10 +140,8 @@ CREATE TABLE `giohang` (
 --
 
 INSERT INTO `giohang` (`id`, `iduser`, `idpro`, `name`, `img`, `giatien`, `soluong`, `iddonhang`) VALUES
-(1, 1, 4, 'Quần bò', '15.jpg', 200000, 1, 1),
-(2, 1, 1, 'Áo Adidas', '1.jpg', 140000, 1, 1),
-(3, 1, 5, 'Áo sơ mi', '24.jpg', 140000, 1, 1),
-(10, 1, 3, 'Giày đen', 'tải xuống.webp', 299000, 1, 7);
+(1, 4, 4, 'Quần bò', '15.jpg', 200000, 2, 8),
+(2, 4, 5, 'Áo sơ mi', '24.jpg', 140000, 3, 9);
 
 -- --------------------------------------------------------
 
@@ -164,7 +166,7 @@ CREATE TABLE `khachhang` (
 
 INSERT INTO `khachhang` (`id`, `username`, `password`, `email`, `diachi`, `sdt`, `trangthai`, `idvaitro`) VALUES
 (1, 'tienlienha123', 'tien2004', 'tienlevi@gmail.com', 'Thanh Trì - Hà Nội', '09283812', 0, 1),
-(2, 'klose', '12345', 'dot@gmail.com', 'Hà Nội', '09', 1, 3),
+(2, 'klose', '12345', 'dot@gmail.com', 'Hà Nội', '09', 1, 2),
 (3, 'muller', '12345', 'muller@gmail.com', 'Đan Phượng - Hà Nội', '0987231231', 0, 2),
 (4, 'rooney', '11111', 'rooney@gmail.com', 'Hà Nội', '09837213111', 0, 2),
 (7, 'nguyen', 'a', 'chuling@gmail.com', 'a', '0987231231', 0, 2),
@@ -200,8 +202,7 @@ INSERT INTO `sanpham` (`id`, `name`, `giatien`, `img`, `mota`, `iddm`, `luotxem`
 (4, 'Quần bò', 200000, '15.jpg', 'LÀM GÌ THÌ LÀM - NHỚ XEM MU', 2, 0, '04:35:58pm 24/11/2023', 0),
 (5, 'Áo sơ mi', 140000, '24.jpg', 'LÀM GÌ THÌ LÀM - NHỚ XEM Chelsea', 1, 0, '04:36:38pm 24/11/2023', 0),
 (6, 'Áo polo xanh', 140000, '33.jpg', 'awdawdawdawdasdawd', 1, 0, '04:38:16pm 24/11/2023', 0),
-(7, 'Giày adidas', 140000, 'shopping.webp', 'Theo nguồn tin từ Ả Rập, Man City sẵn sàng bán Kevin De Bruyne cho Al Nassr vào mùa hè tới.', 3, 0, '0', 6),
-(8, 'Giày adidas', 140000, 'shopping.webp', 'Theo nguồn tin từ Ả Rập, Man City sẵn sàng bán Kevin De Bruyne cho Al Nassr vào mùa hè tới.', 3, 0, '06:05:24pm 28/11/2023', 0);
+(7, 'Giày adidas', 140000, 'shopping.webp', 'Theo nguồn tin từ Ả Rập, Man City sẵn sàng bán Kevin De Bruyne cho Al Nassr vào mùa hè tới.', 3, 0, '0', 6);
 
 -- --------------------------------------------------------
 
@@ -289,7 +290,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT cho bảng `binhluan`
 --
 ALTER TABLE `binhluan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT cho bảng `danhmuc`
@@ -301,13 +302,13 @@ ALTER TABLE `danhmuc`
 -- AUTO_INCREMENT cho bảng `donhang`
 --
 ALTER TABLE `donhang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `giohang`
 --
 ALTER TABLE `giohang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `khachhang`
