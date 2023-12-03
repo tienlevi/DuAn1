785fee5ee56d0f8e723fa6bcc6757f32e9b87f31
TiennLevii <nguyentrachtien2401@gmail.com>
2023-12-03 15:06:13 +0700

    Update Binhluan File

--- admin/sanpham/binhluan.php
+++ admin/sanpham/binhluan.php
@@ -3,8 +3,11 @@
   include "../../model/binhluan.php";
   $idpro = $_REQUEST['idpro'];
   $listbl = loadAllBl($idpro);
-  if(isset($_GET['id']) && ($_GET['id'] > 0)){
-    anBinhLuan($_GET['id']);
+  if(isset($_GET['idan']) && ($_GET['idan'] > 0)){
+    anBinhLuan($idpro,$_GET['idan']);
+  }
+  if(isset($_GET['idhien']) && ($_GET['idhien'] > 0)){
+    hienBinhLuan($idpro,$_GET['idhien']);
   }
 ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
@@ -28,10 +31,28 @@
                 <td>'.$iduser.'</td>
                 <td>'.$noidung.'</td>
                 <td>'.$ngaybinhluan.'</td>    
-                <td><a href="binhluan.php?idpro='.$idpro.'&id='.$id.'">'.($trangthai === 0 ? "<p class='btn btn-danger'>Ẩn bình luận</p>" : "<p class='btn btn-success'>Hiện bình luận</p>").'</a></td>
+                <td>'.($trangthai === 0 ? "<a href='binhluan.php?idpro=$idpro&idan=$id' onclick='return HideComment()' class='btn btn-danger'>Ẩn bình luận</a>" 
+                : "<a href='binhluan.php?idpro=$idpro&idhien=$id' onclick='return ShowComment()' class='btn btn-success'>Hiện bình luận</a>").'</td>
                 </tr>';
                 }
               ?>
         </table>
     </div>
 </div>
+<script>
+function HideComment() {
+    if (confirm("Bạn có muốn ẩn bình luận này không ?")) {
+        alert("Ẩn bình luận thành công")
+    } else {
+        return false;
+    }
+}
+
+function ShowComment() {
+    if (confirm("Bạn có muốn Hiện bình luận này không ?")) {
+        alert("Hiện bình luận thành công")
+    } else {
+        return false;
+    }
+}
+</script>
